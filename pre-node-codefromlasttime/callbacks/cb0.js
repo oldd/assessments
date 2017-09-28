66b3e41d79f8170fc59e963cc046818fe210cf23
[rien.cosyns] <[rien.cosyns@gmail.com]>
2017-09-28 14:04:48 +0200

    fixed small bug

--- pre-node-codefromlasttime/callbacks/cb0.js
+++ pre-node-codefromlasttime/callbacks/cb0.js
@@ -1,18 +1,18 @@
 function devSide(err, result) {
-	// code this
-};
+  // code this
+}
 
 function adder(number1, number2, cb) {
-	var result = 0000;
-	var err = null;
-	if((typeof number1 == 'number') && (typeof number2 == 'number')) {
-		result = number1 + number2;
-	} else {
-		err = 'arguments arent numbers';
-	};
-	cb(err, result);
-};
+  var result = 0000;
+  var err = null;
+  if (typeof number1 == "number" && typeof number2 == "number") {
+    result = number1 + number2;
+  } else {
+    err = "arguments arent numbers";
+  }
+  cb(err, result);
+}
 
-moduleSide(5, 4, devSide);  // -> 9
+adder(5, 4, devSide); // -> 9
 
-moduleSide('tim', 4, devSide);  // -> arguments arent numbers
+adder("tim", 4, devSide); // -> arguments arent numbers
